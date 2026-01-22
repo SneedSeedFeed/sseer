75855ea9a3e09698ca13451980c99de9f9454b8e
Adam <adam.stopher@protonmail.com>
2026-01-22 22:48:12 +0000

    Expanded errors for reqwest impl Removed stupid comment Removed even stupider todo!()

--- src/reqwest.rs
+++ src/reqwest.rs
@@ -1,4 +1,6 @@
 use core::{
+    error::Error,
+    fmt::{Display, Formatter},
     pin::Pin,
     task::{Context, Poll, ready},
     time::Duration,
@@ -17,7 +19,7 @@ use reqwest::{
 
 use crate::{
     constants::EMPTY_STR,
-    errors::CantCloneError,
+    errors::{CantCloneError, EventStreamError},
     event::Event,
     event_stream::EventStream,
     retry::{DEFAULT_RETRY, ExponentialBackoff, RetryPolicy},
@@ -27,6 +29,12 @@ use crate::{
 pub enum StreamEvent {
     Open,
     Event(Event),
+}
+
+impl From<Event> for StreamEvent {
+    fn from(event: Event) -> Self {
+        StreamEvent::Event(event)
+    }
 }
 
 pin_project! {
@@ -69,19 +77,16 @@ fn response_to_stream(response: Response) -> EventStream<BodyDataStream<Body>> {
     EventStream::new(BodyDataStream::new(Body::from(response)))
 }
 
-pub enum SseError {
-    InvalidLastEventId(Str),
-}
-
 impl<'pin, R> EventSourceProjection<'pin, R> {
     fn initiate_connection(
         &mut self,
         retry_state: Option<(usize, Duration)>,
-    ) -> Result<(), SseError> {
+    ) -> Result<(), EventSourceErrorKind> {
         let req = self.builder.try_clone().unwrap().header(
             HeaderName::from_static("last-event-id"),
-            HeaderValue::from_str(self.last_event_id)
-                .map_err(|_| SseError::InvalidLastEventId(self.last_event_id.clone()))?,
+            HeaderValue::from_str(self.last_event_id).map_err(|_| {
+                EventSourceErrorKind::InvalidLastEventId(self.last_event_id.clone())
+            })?,
         );
         let res_future = Box::pin(req.send());
         *self.connection_state = ConnectionState::Connecting {
@@ -122,6 +127,16 @@ impl<'pin, R> EventSourceProjection<'pin, R> {
             self.connection_state.set(ConnectionState::Closed);
         }
     }
+
+    fn handle_event(&mut self, event: &Event)
+    where
+        R: RetryPolicy,
+    {
+        *self.last_event_id = event.id.clone();
+        if let Some(duration) = event.retry {
+            self.retry_policy.set_reconnection_time(duration)
+        }
+    }
 }
 
 impl<R> EventSource<R> {
@@ -141,49 +156,134 @@ impl<R> EventSource<R> {
     }
 }
 
+#[derive(Debug)]
+enum EventSourceErrorKind {
+    InvalidLastEventId(Str),
+    Transport(ReqwestError),
+    Stream(EventStreamError<ReqwestError>),
+    StreamEnded, // not sure how i feel about this being an error tbh, change me?
+}
+
+impl Display for EventSourceErrorKind {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        match self {
+            EventSourceErrorKind::InvalidLastEventId(s) => s.fmt(f),
+            EventSourceErrorKind::Transport(err) => err.fmt(f),
+            EventSourceErrorKind::Stream(err) => err.fmt(f),
+            EventSourceErrorKind::StreamEnded => "stream ended".fmt(f),
+        }
+    }
+}
+
+impl Error for EventSourceErrorKind {}
+
+pub struct EventSourceError {
+    retry_state: Option<(usize, Duration)>,
+    kind: EventSourceErrorKind,
+}
+
+impl Display for EventSourceError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let retry_state = self
+            .retry_state
+            .as_ref()
+            .map(|(attempts, duration)| format!("attempts: '{attempts}' duration: {duration:?}"))
+            .unwrap_or(String::from("no last retry"));
+        write!(
+            f,
+            "Error '{kind}' with last retry: {retry_state}",
+            kind = self.kind
+        )
+    }
+}
+
+impl EventSourceError {
+    fn new(
+        kind: impl Into<EventSourceErrorKind>,
+        retry_state: impl Into<Option<(usize, Duration)>>,
+    ) -> Self {
+        Self {
+            retry_state: retry_state.into(),
+            kind: kind.into(),
+        }
+    }
+}
+
+impl From<ReqwestError> for EventSourceErrorKind {
+    fn from(value: ReqwestError) -> Self {
+        Self::Transport(value)
+    }
+}
+
+impl From<EventStreamError<ReqwestError>> for EventSourceErrorKind {
+    fn from(value: EventStreamError<ReqwestError>) -> Self {
+        Self::Stream(value)
+    }
+}
+
 impl<R> futures_core::Stream for EventSource<R>
 where
     R: RetryPolicy,
 {
-    type Item = Result<StreamEvent, reqwest::Error>;
+    type Item = Result<StreamEvent, EventSourceError>;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let mut this = self.project();
-        match this.connection_state.as_mut().project() {
-            ConnectionStateProjection::Connecting {
-                future,
-                retry_state,
-            } => {
-                let retry_state = *retry_state;
-                match ready!(future.poll(cx)) {
-                    Ok(response) => {
-                        this.handle_successful_response(response, retry_state);
-                        return Poll::Ready(Some(Ok(StreamEvent::Open)));
+        loop {
+            match this.connection_state.as_mut().project() {
+                ConnectionStateProjection::Connecting {
+                    future,
+                    retry_state,
+                } => {
+                    let retry_state = *retry_state;
+                    match ready!(future.poll(cx)) {
+                        Ok(response) => {
+                            this.handle_successful_response(response, retry_state);
+                            return Poll::Ready(Some(Ok(StreamEvent::Open)));
+                        }
+                        Err(err) => {
+                            this.connection_state.set(ConnectionState::Closed);
+                            return Poll::Ready(Some(Err(EventSourceError::new(err, retry_state))));
+                        }
                     }
-                    Err(e) => {
+                }
+                ConnectionStateProjection::Retrying {
+                    delay,
+                    attempt_number,
+                    delay_duration,
+                } => {
+                    ready!(delay.poll(cx));
+                    let retry_state = Some((*attempt_number, *delay_duration));
+                    if let Err(err) = this.initiate_connection(retry_state) {
                         this.connection_state.set(ConnectionState::Closed);
-                        return Poll::Ready(Some(Err(e)));
+                        return Poll::Ready(Some(Err(EventSourceError::new(err, retry_state))));
                     }
                 }
-            }
-            ConnectionStateProjection::Retrying {
-                delay,
-                attempt_number,
-                delay_duration,
-            } => todo!(),
-            ConnectionStateProjection::Open {
-                stream,
-                retry_state,
-            } => {
-                let retry_state = *retry_state;
-                match ready!(stream.poll_next(cx)) {
-                    Some(Ok(bytes)) => {}
-                    Some(Err(e)) => {}
-                    None => todo!(),
+                ConnectionStateProjection::Open {
+                    stream,
+                    retry_state,
+                } => {
+                    let retry_state = *retry_state;
+                    match ready!(stream.poll_next(cx)) {
+                        Some(Ok(event)) => {
+                            this.handle_event(&event);
+                            return Poll::Ready(Some(Ok(event.into())));
+                        }
+                        Some(Err(err)) => {
+                            this.handle_error(retry_state);
+                            return Poll::Ready(Some(Err(EventSourceError::new(err, retry_state))));
+                        }
+                        None => {
+                            this.handle_error(retry_state);
+                            return Poll::Ready(Some(Err(EventSourceError::new(
+                                EventSourceErrorKind::StreamEnded,
+                                retry_state,
+                            ))));
+                        }
+                    }
                 }
+                ConnectionStateProjection::Closed => return Poll::Ready(None),
             }
-            ConnectionStateProjection::Closed => return Poll::Ready(None),
         }
-        todo!()
     }
 }
